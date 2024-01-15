fd1b3faf29eeff79798231424e50320e65fd3233
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-15 18:40:14 -0300

    :boom: Fix: Correção de nome do banco de dados

---
+++ src/config/db.js
@@ -0,0 +1,18 @@
+const { Pool } = require("pg");
+
+const user = process.env.POSTGRES_USER;
+const password = process.env.POSTGRES_PASSWORD;
+const host = process.env.POSTGRES_HOST;
+const port = process.env.POSTGRES_PORT;
+const database = process.env.POSTGRES_DB;
+
+const pool = new Pool({
+user,
+  host,
+  database,
+  password,
+  port,
+  ssl: false,
+});
+
+module.exports = pool;
