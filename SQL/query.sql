6de646087a33407245dc23b9212bfa892cf2795a
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-15 17:33:03 -0300

    :tada: Init: Estrutura inicial

---
+++ SQL/query.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE PAYMENTSAPP
+-- APÓS CRIAR O BANCO DE DADOS CRIE AS TABELAS A SEGUIR:
+
+
+
+-- TABELA DE USUARIOS
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL
+);
+
+-- TABELA DE SALDOS
+CREATE TABLE saldos (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    valor_inicial DECIMAL(10, 2) NOT NULL,
+    valor_restante DECIMAL(10, 2) NOT NULL
+);
+-- TABELA DE PAGAMENTOS
+CREATE TABLE pagamentos (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    valor DECIMAL(10, 2) NOT NULL,
+    saldo_id INT REFERENCES saldos(id) ON DELETE CASCADE
+);
+
+
+-- TABELA DE PAGAMENTOS DOS USUARIOS
+CREATE TABLE pagamentos_usuarios (
+    pagamento_id INT REFERENCES pagamentos(id) ON DELETE CASCADE,
+    usuario_id INT REFERENCES usuarios(id) ON DELETE CASCADE,
+    PRIMARY KEY (pagamento_id, usuario_id)
+);
