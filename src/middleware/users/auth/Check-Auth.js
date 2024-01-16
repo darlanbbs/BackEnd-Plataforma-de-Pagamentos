9e92bd7524904b6d0ebffd93e17ebdc30d5e83b3
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-16 13:38:35 -0300

    :sparkles: Feat: Validando se dados são introduzidos/se email existe

--- src/middleware/users/auth/Check-Auth.js
+++ src/middleware/users/auth/Check-Auth.js
@@ -1,8 +1,9 @@
-const { FindUserByEmail } = require("../../repositories/UserRepository");
-const {  invalidData } = require("../../utils/helpers/error-helpers");
-const UserValidationMiddleware = (schema) => {
+const { FindUserByEmail } = require("../../../repositories/UserRepository");
+const { invalidData } = require("../../../utils/helpers/error-helpers");
+
+const CheckAuth = (schema) => {
   return async (req, res, next) => {
-    const {email} = req.body
+    const { email } = req.body;
     const { error } = schema.validate(req.body, { abortEarly: true });
     const customError = invalidData();
 
@@ -11,11 +12,14 @@ const UserValidationMiddleware = (schema) => {
       if (errorMessage) {
         return res.status(customError.status).json({ message: customError.message });
       }
-      await FindUserByEmail(email) ? next() : res.status(customError.status).json({message: customError.message });
     }
 
-    
-  };
-};
+    if (!(await FindUserByEmail(email))) {
+      return res.status(customError.status).json({ message: customError.message });
+    }
 
-module.exports = UserValidationMiddleware;
+    next();
+  };
+}
+
+module.exports = CheckAuth;
