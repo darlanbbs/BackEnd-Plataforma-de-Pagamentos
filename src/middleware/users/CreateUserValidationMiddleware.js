25507a4d824366ad607bf5bb343949e821fe5912
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-16 09:38:25 -0300

    :boom: Fix: correção de função de validação de email

--- src/middleware/users/CreateUserValidationMiddleware.js
+++ src/middleware/users/CreateUserValidationMiddleware.js
@@ -1,20 +1,21 @@
 const { emailExists, invalidData } = require("../../utils/helpers/error-helpers");
 
 const CreateUserValidationMiddleware = (schema) => {
-  return (req, res, next) => {
+  return  (req, res, next) => {
+    const {email} = req.body
     const { error } = schema.validate(req.body, { abortEarly: false });
 
     if (error) {
       const errorMessage = error.details[0].message;
-
-      if (errorMessage.includes('email') && errorMessage.includes('already exists')) {
-        const customError = emailExists();
-        return res.status(customError.status).json({ message: customError.message });
-      } else {
+      if (errorMessage) {
         const customError = invalidData();
         return res.status(customError.status).json({ message: customError.message });
-
       }
+    }
+
+    if(emailExists(email)) {
+      const customError = emailExists();
+      return res.status(customError.status).json({ message: customError.message });
     }
 
     next();
