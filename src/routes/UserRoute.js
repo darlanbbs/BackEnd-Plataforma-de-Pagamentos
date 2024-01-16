4388f23e6111cb9a2648a50e6773cb8a978d2340
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-16 10:56:12 -0300

    :sparkles: Feat: Adicionando rota de delete

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -4,10 +4,13 @@ const getUsers = require("../controllers/users/GetUsers");
 const createUser = require("../controllers/users/CreateUser");
 const CreateUserValidationMiddleware = require("../middleware/users/CreateUserValidationMiddleware");
 const { createUserSchema } = require("../validator/UsersSchema/CreateUserSchema");
+const deleteUser = require("../controllers/users/DeleteUser");
+const deleteUserMiddleware = require("../middleware/users/DeleteUserMiddleware");
 ;
 
 
 userRoute.get("/",getUsers)
 userRoute.post("/",CreateUserValidationMiddleware(createUserSchema),createUser)
+userRoute.delete("/:id",deleteUserMiddleware,deleteUser)
 
 module.exports = userRoute;
