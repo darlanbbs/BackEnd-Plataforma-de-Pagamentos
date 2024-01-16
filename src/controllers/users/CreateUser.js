262baeeacfc2a51754a1fec90c89974002da9f0f
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-15 22:50:55 -0300

    :sparkles: Feat: Criação do controller de criação de usuario

---
+++ src/controllers/users/CreateUser.js
@@ -0,0 +1,18 @@
+const pool = require("../../config/db");
+
+
+const createUser = async (req,res) => {
+    const {name, email, password} = req.body
+
+    try {
+        const { rows } = await pool.query(
+          "insert into usuarios (nome, email, senha) values ($1, $2, $3)",
+          [name, email, password]
+        );
+        return res.status(201).json({ message: "Usuário criado com sucesso" });
+      } catch (error) {
+        return res.status(500).json("Erro interno do servidor");
+      }
+}
+
+module.exports = createUser
