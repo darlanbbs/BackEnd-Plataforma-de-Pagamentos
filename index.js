442989fc66a2a5b7dd924c03c4186dcdf142c4fb
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-16 20:16:00 -0300

    :truck: chore:Mudança de nome da rota

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PaymentRoute = require("./src/routes/paymentRoute");
 const userRoute = require("./src/routes/UserRoute");
+const TransactionsRouter = require("./src/routes/TransactiosRoutes");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(userRoute);
-app.use(PaymentRoute);
+app.use(TransactionsRouter);
 
 app.listen(process.env.LISTEN_PORT || "5000");
--- src/routes/paymentRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const PaymentRoute = express();
-
-
-
-
-module.exports = PaymentRoute;
