fd3309bf8b7b0b1d81ee33a63f631296b422c6f6
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-16 16:29:35 -0300

    :recycle: Padronizando erros no codigo

--- src/controllers/users/auth/Login.js
+++ src/controllers/users/auth/Login.js
@@ -1,42 +1,44 @@
-const bcrypt = require('bcrypt');
-const pool = require('../../../config/db');
-const { newToken } = require('../../../services/jwt');
-
+const bcrypt = require("bcrypt");
+const pool = require("../../../config/db");
+const { newToken } = require("../../../services/jwt");
+const { invalidData } = require("../../../utils/helpers/error-helpers");
 
 const loginUser = async (req, res) => {
-    const { email, senha } = req.body;
+  const { email, senha } = req.body;
+  const invalidDataError = invalidData();
+  try {
+    const { rows } = await pool.query(
+      "SELECT id, nome, email, senha FROM usuarios WHERE email = $1",
+      [email]
+    );
 
-    try {
-        const { rows } = await pool.query(
-            "SELECT id, nome, email, senha FROM usuarios WHERE email = $1",
-            [email]
-        );
-
-        if (rows.length === 0) {
-            return res.status(401).json({ message: "Usuário não encontrado." });
-        }
-
-        const match = await bcrypt.compare(senha, rows[0].senha);
-
-        if (!match) {
-            return res.status(401).json({ message: "Os dados fornecidos são inválidos." });
-        }
-
-        const token = await newToken(rows[0].id);
-
-        return res.status(201).json({
-            message: "Usuário conectou com sucesso",
-            usuario: {
-                id: rows[0].id,
-                nome: rows[0].nome,
-                email: rows[0].email
-            },
-            token
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "Erro interno do servidor" });
+    if (rows.length === 0) {
+      return res.status(401).json({ message: "Usuário não encontrado." });
     }
+
+    const match = await bcrypt.compare(senha, rows[0].senha);
+
+    if (!match) {
+      return res
+        .status(invalidDataError.status)
+        .json({ message: invalidDataError.message });
+    }
+
+    const token = await newToken(rows[0].id);
+
+    return res.status(201).json({
+      message: "Usuário conectou com sucesso",
+      usuario: {
+        id: rows[0].id,
+        nome: rows[0].nome,
+        email: rows[0].email,
+      },
+      token,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Erro interno do servidor" });
+  }
 };
 
-module.exports = loginUser
+module.exports = loginUser;
