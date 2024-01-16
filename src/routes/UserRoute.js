5e6ec299dd0c61529fadcd9a40f36b8bd5b92bd9
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-16 09:39:20 -0300

    :sparkles: Feat: criação da rota de criação de usuario

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const getUsers = require("../controllers/users/GetUsers");
 const userRoute = express();
+const getUsers = require("../controllers/users/GetUsers");
+const createUser = require("../controllers/users/CreateUser");
+const CreateUserValidationMiddleware = require("../middleware/users/CreateUserValidationMiddleware");
+const { createUserSchema } = require("../validator/UsersSchema/CreateUserSchema");
+;
 
 
 userRoute.get("/",getUsers)
+userRoute.post("/",CreateUserValidationMiddleware(createUserSchema),createUser)
 
 module.exports = userRoute;
