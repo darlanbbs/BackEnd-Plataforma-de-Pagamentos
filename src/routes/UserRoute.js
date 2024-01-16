6624cc12164f07d2c5af0c7b2a920efcfa0a88e4
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-16 15:13:18 -0300

    :boom: Fix: exportando middleware de autenticaçao

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -3,17 +3,28 @@ const userRoute = express();
 const getUsers = require("../controllers/users/GetUsers");
 const createUser = require("../controllers/users/CreateUser");
 const UserValidationMiddleware = require("../middleware/users/UserValidationMiddleware");
-const { createUserSchema, updateUserSchema } = require("../validator/UsersSchema/UserSchema");
+const {
+  createUserSchema,
+  updateUserSchema,
+} = require("../validator/UsersSchema/UserSchema");
 const deleteUser = require("../controllers/users/DeleteUser");
 const deleteUserMiddleware = require("../middleware/users/DeleteUserMiddleware");
 const updateUser = require("../controllers/users/edit/EditUser");
 const loginUser = require("../controllers/users/auth/Login");
+const {
+  CheckAuth,
+  checkToken,
+} = require("../middleware/users/auth/Check-Auth");
+const authUserSchema = require("../validator/UsersSchema/AuthUserSchema");
 
-
-
-userRoute.get("/",getUsers)
-userRoute.post("/",UserValidationMiddleware(createUserSchema),createUser)
-userRoute.delete("/:id",deleteUserMiddleware,deleteUser)
-userRoute.patch("/update/:id",UserValidationMiddleware(updateUserSchema),updateUser)
-userRoute.post("/login", loginUser)
+userRoute.get("/", getUsers);
+userRoute.post("/", UserValidationMiddleware(createUserSchema), createUser);
+userRoute.post("/login", CheckAuth(authUserSchema), loginUser);
+userRoute.use(checkToken);
+userRoute.delete("/:id", deleteUserMiddleware, deleteUser);
+userRoute.patch(
+  "/update/:id",
+  UserValidationMiddleware(updateUserSchema),
+  updateUser
+);
 module.exports = userRoute;
