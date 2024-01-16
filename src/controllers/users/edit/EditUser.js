13a93b540f697fa2798f6c676dd9c660ac75a10d
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-16 12:35:19 -0300

    :sparkles: Feat: Função de atualização de valores do usuario

---
+++ src/controllers/users/edit/EditUser.js
@@ -0,0 +1,43 @@
+const pool = require("../../../config/db");
+const bcrypt = require('bcrypt');
+
+const updateUser = async (req, res) => {
+    const { nome, email, senha } = req.body;
+    const { id } = req.params;
+
+    let updateFields = [];
+    let params = [];
+    let index = 1;
+
+    if (nome) {
+        updateFields.push(`nome = $${index}`);
+        params.push(nome);
+        index++;
+    }
+
+    if (email) {
+        updateFields.push(`email = $${index}`);
+        params.push(email);
+        index++;
+    }
+
+    if (senha) {
+        const hash = await bcrypt.hash(senha, 10);
+        updateFields.push(`senha = $${index}`);
+        params.push(hash);
+        index++;
+    }
+
+    try {
+        const { rows } = await pool.query(
+            `UPDATE usuarios SET ${updateFields.join(', ')} WHERE id = $${index}`,
+            [...params, id]
+        );
+
+        res.status(200).json({ mensagem: "Usuário atualizado com sucesso" });
+    } catch (error) {
+        return res.status(500).json(error.message);
+    }
+};
+
+module.exports = updateUser;
--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -2,15 +2,16 @@ const express = require("express");
 const userRoute = express();
 const getUsers = require("../controllers/users/GetUsers");
 const createUser = require("../controllers/users/CreateUser");
-const CreateUserValidationMiddleware = require("../middleware/users/CreateUserValidationMiddleware");
-const { createUserSchema } = require("../validator/UsersSchema/CreateUserSchema");
+const UserValidationMiddleware = require("../middleware/users/UserValidationMiddleware");
+const { createUserSchema, updateUserSchema } = require("../validator/UsersSchema/UserSchema");
 const deleteUser = require("../controllers/users/DeleteUser");
 const deleteUserMiddleware = require("../middleware/users/DeleteUserMiddleware");
-;
+const updateUser = require("../controllers/users/edit/EditUser");
+
 
 
 userRoute.get("/",getUsers)
-userRoute.post("/",CreateUserValidationMiddleware(createUserSchema),createUser)
+userRoute.post("/",UserValidationMiddleware(createUserSchema),createUser)
 userRoute.delete("/:id",deleteUserMiddleware,deleteUser)
-
+userRoute.patch("/update/:id",UserValidationMiddleware(updateUserSchema),updateUser)
 module.exports = userRoute;
