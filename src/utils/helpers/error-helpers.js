6af8761da5f9e9a5b9f241850b4a86d5d484f803
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-16 16:25:21 -0300

    :sparkles: Feat: criação de erro de não autorizado

--- src/utils/helpers/error-helpers.js
+++ src/utils/helpers/error-helpers.js
@@ -1,24 +1,31 @@
 module.exports = {
   userNotFound: () => {
-    const err = new Error('Usuário não encontrado.');
+    const err = new Error("Usuário não encontrado.");
     err.status = 404;
-    err.code = 'UsuarioNaoEncontrado';
+    err.code = "UsuarioNaoEncontrado";
     return err;
-},
+  },
 
-  
-    emailExists: () => {
-      const err = new Error('O email já está associado a outra conta de usuário.');
-      err.code = 'EmailUtilizado';
-      err.status = 400;
-      return err;
-    },
-  
-    invalidData: () => {
-      const err = new Error('Os dados fornecidos são inválidos.');
-      err.code = 'DadosInvalidos';
-      err.status = 400;
-      return err;
-    },
-  };
-  
+  unathourizedPermission: () => {
+    const err = new Error("Você não tem permissão para acessar este recurso.");
+    err.status = 401;
+    err.code = "NaoTemPermissao";
+    return err;
+  },
+
+  emailExists: () => {
+    const err = new Error(
+      "O email já está associado a outra conta de usuário."
+    );
+    err.code = "EmailUtilizado";
+    err.status = 400;
+    return err;
+  },
+
+  invalidData: () => {
+    const err = new Error("Os dados fornecidos são inválidos.");
+    err.code = "DadosInvalidos";
+    err.status = 400;
+    return err;
+  },
+};
