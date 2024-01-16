3bac5223c3ec8eac1ed666c3f059642573a83ad5
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-16 20:13:33 -0300

    :sparkles: Feat: Criação da busca dos saldos pelo id do usuario estando logado

---
+++ src/controllers/TransactionController/Balance/GetBalance.js
@@ -0,0 +1,22 @@
+const pool = require("../../../config/db");
+const { verifyToken } = require("../../../services/jwt");
+
+const getBalance = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const query = `
+    SELECT saldos.*
+      FROM saldos
+      INNER JOIN usuarios ON saldos.usuario_id = usuarios.id
+      WHERE usuarios.id = $1`;
+    const values = [id];
+
+    const { rows } = await pool.query(query, values);
+
+    return res.send(rows);
+  } catch (error) {
+    return res.status(500).json(`Erro interno do servidor ${error}`);
+  }
+};
+
+module.exports = { getBalance };
