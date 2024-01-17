51248603d5023ecfdf109e93290a1d4b360e1c18
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-16 21:04:24 -0300

    :sparkles: Feat: criação de middleware de transação com valores padroes do valor_restante

---
+++ src/middleware/TransactionMiddleware/TransactionValidationMiddleware.js
@@ -0,0 +1,36 @@
+const { invalidData } = require("../../utils/helpers/error-helpers");
+
+const addDefaultValue = (data) => {
+  if ("valor_inicial" in data) {
+    data.valor_restante = data.valor_inicial;
+  }
+  return data;
+};
+
+const TransactionValidationMiddleware = (schema) => {
+  return async (req, res, next) => {
+    try {
+      const dataWithDefaultValue = addDefaultValue(req.body);
+
+      const { error } = schema.validate(dataWithDefaultValue, {
+        abortEarly: false,
+      });
+      if (error) {
+        const errorMessage = error.details[0].message;
+        if (errorMessage) {
+          const customError = invalidData();
+          return res
+            .status(customError.status)
+            .json({ message: customError.message });
+        }
+      }
+
+      req.body = dataWithDefaultValue;
+      next();
+    } catch (error) {
+      return res.status(500).json(`Erro interno do servidor ${error}`);
+    }
+  };
+};
+
+module.exports = TransactionValidationMiddleware;
--- src/routes/TransactiosRoutes.js
+++ src/routes/TransactiosRoutes.js
@@ -4,7 +4,20 @@ const { checkToken } = require("../middleware/users/auth/Check-Auth");
 const {
   getBalance,
 } = require("../controllers/TransactionController/Balance/getBalance");
+const TransactionValidationMiddleware = require("../middleware/TransactionMiddleware/TransactionValidationMiddleware");
+const {
+  createSaldoSchema,
+} = require("../validator/TransactionsSchema/TransactionsSchema");
+const {
+  createBalance,
+} = require("../controllers/TransactionController/Balance/CreateBalance");
 const TransactionsRouter = express();
 
-TransactionsRouter.get("/balance/:id", checkToken, getBalancealance);
+TransactionsRouter.get("/balance/:id", checkToken, getBalance);
+TransactionsRouter.post(
+  "/balance/:id",
+  checkToken,
+  TransactionValidationMiddleware(createSaldoSchema),
+  createBalance
+);
 module.exports = TransactionsRouter;
