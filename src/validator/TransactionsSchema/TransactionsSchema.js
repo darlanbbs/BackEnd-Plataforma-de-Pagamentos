56606f0717914f531ddbd855a1c1d6ac31b5ed16
Darlan Bomfim <darlanbs05@gmail.com>
2024-01-16 21:03:52 -0300

    :sparkles: Feat: criação de schema para criação de saldo

--- src/validator/TransactionsSchema/TransactionsSchema.js
+++ src/validator/TransactionsSchema/TransactionsSchema.js
@@ -1,17 +1,16 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const createSaldoSchema = Joi.object({
-    nome: Joi.string().max(30).required(),
-    descricao: Joi.string().allow('').max(255), 
-    valor_inicial: Joi.number().positive().precision(2).required(),
-    valor_restante: Joi.number().positive().precision(2).required(),
-  });
-  
-  const createPagamentoSchema = Joi.object({
-      nome: Joi.string().max(30).required(),
-      descricao: Joi.string().allow('').max(255), 
-      valor: Joi.number().positive().precision(2).required(),
-      saldo_id: Joi.number().integer().positive().required(),
-    });
-  
-  module.exports = {createSaldoSchema,createPagamentoSchema};
+  nome: Joi.string().min(3).required(),
+  descricao: Joi.string().allow("").max(255),
+  valor_inicial: Joi.number().positive().precision(2).required(),
+  valor_restante: Joi.allow(""),
+});
+const createPagamentoSchema = Joi.object({
+  nome: Joi.string().max(30).required(),
+  descricao: Joi.string().allow("").max(255),
+  valor: Joi.number().positive().precision(2).required(),
+  saldo_id: Joi.number().integer().positive().required(),
+});
+
+module.exports = { createSaldoSchema, createPagamentoSchema };
